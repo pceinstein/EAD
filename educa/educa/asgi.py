af93a8635bca72884e91c8be7dfffb6e7763f4dc
pceinstein <pceinstein@hotmail.com>
2024-02-18 16:04:30 -0300

    preparando para o lançamento

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -4,23 +4,24 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
-
+'''
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seuprojeto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     # Adicione rotas de WebSocket aqui, se necessário.
 })
+'''
 
 
 """
 https://github.com/django/channels/issues/2035
 """
-"""
+
 import os
 
 import django
@@ -29,4 +30,3 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
 django.setup()
 application = get_default_application()
-"""
--- educa/educa/routing.py
+++ educa/educa/routing.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    #"http": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
